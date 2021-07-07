2c433b08b43da3a37deb63c1261cd0e3c05e78a2
Subbareddy Nallamachu <subbareddynallamachu@gmail.com>
2021-07-07 06:47:10 -0700

    Hello World example
    Starting Hello World application with VUE

---
+++ hello-world/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="my-app">
+    <h1 class="h1">Hello World</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style>
+.h1{
+  color: blue;
+}
+</style>
