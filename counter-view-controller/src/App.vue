77d56f2f0d9bd1fc793cff729c52acb5dfbcfbbd
Subbareddy Nallamachu <subbareddynallamachu@gmail.com>
2021-07-07 08:34:03 -0700

    Updated version of Counter-View
    Counter-View-Controller is an updated version of Counter-view project

---
+++ counter-view-controller/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="my-app">
+    <!-- :counter is equal to v-bind:counter -->
+    <counter-view v-bind:counter="counter"></counter-view>
+    <counter-controller @on-increment="handleIncrement" @on-reset="handleReset"></counter-controller>
+  </div>
+</template>
+
+<script>
+import CounterView from './components/CounterView';
+import CounterController from './components/CounterController';
+export default {
+  components:{
+    CounterView,CounterController
+  },
+  data() {
+    return {
+      counter: 0
+    }
+  },
+  methods: {
+    handleIncrement(val){
+      this.counter +=val
+    },
+    handleReset(){
+      this.counter = 0
+    }
+  },
+}
+</script>
+
+<style>
+.my-app{
+  color: blue;
+  padding: 200px;
+}
+</style>
---
+++ counter-view-controller/src/components/CounterController.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <!-- @click is equal to v-on:click -->
+        <button @click="$emit('on-increment',1)">Increment</button>
+        <button @click="$emit('on-increment',-1)">Decrement</button>
+        <!-- Here is an example of method calling on click event -->
+        <button @click="reset">Reset</button>
+    </div>
+</template>
+
+<script>
+export default {
+    methods: {
+        reset(){
+            //this.counter=0
+            this.$emit('on-reset',0)
+        }
+    }
+}
+</script>
